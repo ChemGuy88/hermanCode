b66ecf51992eda012bb5439b90c4fbd6a1f3f53c
Herman <herman@herman-imac.attlocal.net>
2024-03-10 19:44:17 -0400

    Major update: Add optional argument parsing to process many keywords

--- macOS/uninstallers/findTraces.bash
+++ macOS/uninstallers/findTraces.bash
@@ -2,7 +2,93 @@
 
 # REMEMBER, this script must be run as `sudo` to avoid getting access denied errors when running `find`.
 
-source /Users/herman/.hermanCode/macOS/functions.bash
+# shellcheck source="/Users/herman/.hermanCode/macOS/functions.bash"
+source "/Users/herman/.hermanCode/macOS/functions.bash"
+
+# Formatting
+# bold=$(tput bold)
+# normal=$(tput sgr0)
+RED=$'\e[0;31m'
+# GRN=$'\e[0;32m'
+# ORN=$'\e[0;33m'
+NC=$'\e[0m'
+
+usage0() {
+    cat <<USAGE
+
+    $0 -k <TRACE_KEYWORDS> -t TRUE|FALSE
+    -k The keyword to search for. Use multiple -k for search for multiple keywords.
+    -t The script test flag. One of TRUE or FALSE.
+USAGE
+}
+
+usage() {
+    usage0 1>&2
+    exit 1
+}
+
+
+# >>> Argument parsing >>>
+DIRECTORY="/"
+while getopts ":k:d:t:" opt; do
+    case "${opt}" in
+        k) TRACE_KEYWORDS+=("$OPTARG");;
+        d) DIRECTORY=${OPTARG};;
+        t) TEST_MODE=${OPTARG};;
+        *) usage;;
+    esac
+done
+shift $((OPTIND -1))
+
+if [ -z "${TRACE_KEYWORDS[*]}" ] || [ -z "${TEST_MODE}" ] || [ -z "${DIRECTORY}" ]; then
+    usage
+fi
+# <<< Argument parsing <<<
+
+# >>> Argument confirmation >>>
+SHOULD_EXIT=0
+
+# $TRACE_KEYWORDS
+if [[ -z "$TRACE_KEYWORDS" ]]; then
+    echo "${RED}You must supply a keyword to search for${NC}."
+    SHOULD_EXIT=1
+fi
+
+# $DIRECTORY
+if [[ -z "$DIRECTORY" ]]; then
+    echo "${RED}You must supply the directory path to search in${NC}."
+    SHOULD_EXIT=1
+fi
+
+# $TEST_MODE
+MESSAGE="${RED}TEST_MODE must be one of {TRUE, FALSE}${NC}."
+if [ -z "$TEST_MODE" ]; then
+    echo "$MESSAGE"
+    SHOULD_EXIT=1
+else
+    if [ "$TEST_MODE" == "TRUE" ] || [ "$TEST_MODE" == "FALSE" ]; then
+        :
+    else
+        echo "$MESSAGE"
+        SHOULD_EXIT=1
+    fi
+fi
+
+# Argument confirmation
+echo "TRACE_KEYWORDS:"
+for val in "${TRACE_KEYWORDS[@]}"; do
+    echo "  - $val"
+done
+echo "DIRECTORY = ${DIRECTORY}"
+echo "TEST_MODE = ${TEST_MODE}"
+
+if [ "$SHOULD_EXIT" -eq 0 ]; then
+    :
+else
+    exit $SHOULD_EXIT
+fi
+# <<< Argument confirmation <<<
+
 
 FIND_TRACES_DIRECTORY=~/"Find Traces/$(getTimestamp)"
 if ! [ -d "$FIND_TRACES_DIRECTORY" ]; then
@@ -14,21 +100,14 @@ fi
 
 echo "This directory was created by 'findTraces.sh'" > "$FIND_TRACES_DIRECTORY/README.md"
 
-nohup find / -iname *canopy* 1> "$FIND_TRACES_DIRECTORY/canopy.out" 2> "$FIND_TRACES_DIRECTORY/canopy.err" &
-nohup find / -iname *charles* 1> "$FIND_TRACES_DIRECTORY/charles.out" 2> "$FIND_TRACES_DIRECTORY/charles.err" &
-nohup find / -iname *CloudFlare* 1> "$FIND_TRACES_DIRECTORY/CloudFlare.out" 2> "$FIND_TRACES_DIRECTORY/CloudFlare.err" &
-nohup find / -iname *CrimeFlare* 1> "$FIND_TRACES_DIRECTORY/CrimeFlare.out" 2> "$FIND_TRACES_DIRECTORY/CrimeFlare.err" &
-nohup find / -iname *iStats* 1> "$FIND_TRACES_DIRECTORY/iStats.out" 2> "$FIND_TRACES_DIRECTORY/iStats.err" &
-nohup find / -iname *testgen* 1> "$FIND_TRACES_DIRECTORY/testgen.out" 2> "$FIND_TRACES_DIRECTORY/testgen.err" &
-# nohup find / -iname *mactex* 1> "$FIND_TRACES_DIRECTORY/mactex.out" 2> "$FIND_TRACES_DIRECTORY/mactex.err" &
-# nohup find / -iname *matlab* 1> "$FIND_TRACES_DIRECTORY/matlab.out" 2> "$FIND_TRACES_DIRECTORY/matlab.err" &
-# nohup find / -iname *qgl* 1> "$FIND_TRACES_DIRECTORY/libQGL.out" 2> "$FIND_TRACES_DIRECTORY/libQGL.err" &
-# nohup find / -iname *weka* 1> "$FIND_TRACES_DIRECTORY/weka.out" 2> "$FIND_TRACES_DIRECTORY/weka.err" &
-# nohup find / -iname *anaconda* 1> "$FIND_TRACES_DIRECTORY/anaconda.out" 2> "$FIND_TRACES_DIRECTORY/anaconda.err" &
-# nohup find / \(! -iname *atomic* -iname *atom*\) 1> "$FIND_TRACES_DIRECTORY/atom.out" 2> "$FIND_TRACES_DIRECTORY/anaconda.err" &
-# nohup find / \(! -iname *miniconda* ! -iname *anaconda* -iname *conda*\) 1> "$FIND_TRACES_DIRECTORY/conda.out" 2> "$FIND_TRACES_DIRECTORY/anaconda.err" &
-# nohup find / \( -iname *brew* ! -iname *hebrew* \) 1> "$FIND_TRACES_DIRECTORY/homebrew.out" 2> "$FIND_TRACES_DIRECTORY/homebrew.err" &
-# nohup find / -iname *miniconda* 1> "$FIND_TRACES_DIRECTORY/miniconda.out" 2> "$FIND_TRACES_DIRECTORY/miniconda.err" &
-# nohup find / \( -iname *R-project* \) 1> "$FIND_TRACES_DIRECTORY/R-project.out" 2> "$FIND_TRACES_DIRECTORY/R-project.err" &
+for keyword in "${TRACE_KEYWORDS[@]}";
+do
+    echo "  $keyword"
+    if [ "$TEST_MODE" == "FALSE" ]; then
+        nohup find "$DIRECTORY" -iname *"$keyword"* 1> "$FIND_TRACES_DIRECTORY/$keyword.out" 2> "$FIND_TRACES_DIRECTORY/$keyword.err" &
+    else
+        :
+    fi
+done
 
 echo "Scripts are running."
