fd191813904adcd5d0fa84ce72a7257397372d71
Herman <herman@herman-imac.attlocal.net>
2024-03-17 22:01:15 -0400

    Add "processTraces-Multiple.bash"

---
+++ macOS/uninstallers/processTraces-Multiple.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+macOS/uninstallers/processTraces.bash -t FALSE -f "Find Traces/2024-03-09 20-19-34/python.out"
+macOS/uninstallers/processTraces.bash -t FALSE -f "Find Traces/2024-03-10 19-37-27/vmware.out"
+macOS/uninstallers/processTraces.bash -t FALSE -f "Find Traces/2024-03-10 21-34-23/akamai.out"
+macOS/uninstallers/processTraces.bash -t FALSE -f "Find Traces/2024-03-10 21-34-23/bluestack.out"
+macOS/uninstallers/processTraces.bash -t FALSE -f "Find Traces/2024-03-10 21-34-23/continuum.out"
+macOS/uninstallers/processTraces.bash -t FALSE -f "Find Traces/2024-03-10 21-34-23/itools.out"
+macOS/uninstallers/processTraces.bash -t FALSE -f "Find Traces/2024-03-10 21-34-23/signalscope.out"
+macOS/uninstallers/processTraces.bash -t FALSE -f "Find Traces/2024-03-10 21-34-23/teams.out"
+macOS/uninstallers/processTraces.bash -t FALSE -f "Find Traces/2024-03-10 21-34-23/testgen.out"
+macOS/uninstallers/processTraces.bash -t FALSE -f "Find Traces/2024-03-10 21-34-23/texlive.out"
+macOS/uninstallers/processTraces.bash -t FALSE -f "Find Traces/2024-03-10 21-34-23/texmf.out"
+macOS/uninstallers/processTraces.bash -t FALSE -f "Find Traces/2024-03-10 21-34-23/texshop.out"
+macOS/uninstallers/processTraces.bash -t FALSE -f "Find Traces/2024-03-10 21-34-23/textwrangler."out
