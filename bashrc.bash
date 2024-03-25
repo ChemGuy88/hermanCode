c4087479fdabbd1446e7b8c6c146128667d8483c
Herman Autore <herman@ufl.edu>
2024-03-24 22:00:20 -0400

    Add gate-keeping

--- bashrc.bash
+++ bashrc.bash
@@ -1,4 +1,38 @@
 ################################################################################
+### Gate-keeping ###############################################################
+################################################################################
+
+RED=$'\e[0;31m'
+GRN=$'\e[0;32m'
+NC=$'\e[0m'
+
+TheWorldIsFlat=false  # Change this to `true` to gate-keep the gate keeper.
+if [[ "$TheWorldIsFlat" = true ]]; then
+    read -p "Do you want to ${RED}continue${NC} loading BASH run commands? [Y/n]: " confirm
+
+    if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+        :
+        echo "${GRN}Continuing${NC} to load BASH RC."
+    else
+        echo "${RED}Aborting${NC} the load of BASH RC."
+        return
+    fi
+fi
+
+# Continue sourcing file if we're logging in interactively. Otherwise, return 
+# early. This is to prevent polluting the shell with un-needed output.
+# This is necessary when running `scp`.
+# See https://unix.stackexchange.com/a/505642/399435
+
+if [[ $- == *i*  ]]; then
+    :  # Continue reading BASH RC
+    echo "Detected ${GRN}interactive${NC} mode. ${GRN}Continuing${NC} to load BASH RC."
+else
+    # echo "Detected ${RED}non-interactive${NC} mode. ${RED}Aborting${NC} the load of BASH RC."
+    return
+fi
+
+################################################################################
 ### Constants: Define values ###################################################
 ################################################################################
 
@@ -42,29 +76,7 @@ fi
 ### Functions ##################################################################
 ################################################################################
 
-listpath () {
-     awk -F\: '{
-     for (i = 0; ++i <= NF;)
-          print "Path", i,":", $i
-     }' <<<$1
-}
-# listpath $PATH
-# listpath $PYTHONPATH
-
-timeStamp() {
-date "+%Y-%m-%d %H-%M-%S"
-}
-
-getDirectorySizes () {
-    currentDirectory=$(pwd)
-    cd $1
-    du -sh */
-    cd "$currentDirectory"
-}
-
-getPgrep() {
-    pgrep -U herman -f $1 | xargs --no-run-if-empty ps fp
-}
+source "/home/herman/.hermanCode/functions.bash"
 
 ################################################################################
 ### Shortcuts ##################################################################
@@ -73,10 +85,9 @@ getPgrep() {
 # Aliases
 alias vim="vi -S ~/.vim/.vimrc"
 alias cddr='cd "/data/herman/mnt/ufhsd/SHANDS/SHARE/DSS/IDR Data Requests/ACTIVE RDRs/Bian/IRB201902162"'
-alias cddr='cd "/data/herman/mnt/ufhsd/SHANDS/SHARE/DSS/IDR Data Requests/ACTIVE RDRs/Bian/IRB202003281"'
 alias cddr='cd "/data/herman/mnt/ufhsd/SHANDS/SHARE/DSS/IDR Data Requests/ACTIVE RDRs/Liu/IRB202300703"'
 alias cddr='cd "/data/herman/Projects/IRB202300703"'
-alias cdproject='cd "/data/herman/Documents/GitHub/Company Ratings Scraper"'
+alias cddr='cd "/data/herman/mnt/ufhsd/SHANDS/SHARE/DSS/IDR Data Requests/ACTIVE RDRs/Bian/IRB202003281"'
 alias mountufhsd='sudo /sbin/mount.cifs //ahcdfs.ahc.ufl.edu/files /data/herman/mnt/ufhsd -o user=herman,dom=ad.ufl.edu,uid=herman,gid=herman,password=$HFA_UFADPWD,sec=ntlmssp,vers=2.0'
 alias mountufhsd='sudo /sbin/mount.cifs //ahcdfs.ahc.ufl.edu/files "/data/herman/Mounted Drives/UF Health Shared Drive" -o user=herman,dom=ad.ufl.edu,uid=herman,gid=herman,password=$HFA_UFADPWD,sec=ntlmssp,vers=2.0'
 
@@ -96,3 +107,7 @@ cdherman(){
 # Conveniences
 cdwork
 getPgrep ".py"
+echo
+df -h /data/herman/Projects
+echo
+du -sh /data/herman/Projects  # for el in /data/herman; if el not "mnt"; du -sh el
