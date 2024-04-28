90a39187bb4cb3878c052c051e62b0efb26c06cf
Herman Autore <herman@ufl.edu>
2024-04-27 21:12:48 -0400

    Update `getPgrep`, add shellcheck suggestions

--- functions.bash
+++ functions.bash
@@ -2,7 +2,7 @@ listpath () {
      awk -F\: '{
      for (i = 0; ++i <= NF;)
           print "Path", i,":", $i
-     }' <<<$1
+     }' <<<"$1"
 }
 # listpath $PATH
 # listpath $PYTHONPATH
@@ -13,12 +13,11 @@ date "+%Y-%m-%d %H-%M-%S"
 
 getDirectorySizes () {
     currentDirectory=$(pwd)
-    cd $1
+    cd "$1" || return
     du -sh */
-    cd "$currentDirectory"
+    cd "$currentDirectory" || return
 }
 
 getPgrep() {
-    pgrep -U herman -f $1 | xargs --no-run-if-empty ps fp
+    pgrep -U "$1" -f "$2" | xargs --no-run-if-empty ps f -o lstart -o "%p %r %y %x %a" --sort +pid p
 }
-
