97d7a37500df224905fe6a42d9a92f86f53859b9
Herman <herman@herman-imac.attlocal.net>
2024-04-27 20:59:43 -0400

    Generalize for portability

--- Shell Package/macOS/logout.bash
+++ Shell Package/macOS/logout.bash
@@ -1,3 +1,5 @@
-if [ -f ~/.hermanCode/limericks_out.bash ]; then
-    source ~/.hermanCode/limericks_out.bash
+LIMERICKS_OUT_PATH="$HERMAN_CODE_DIR/limericks_out.bash"
+if [ -f "$LIMERICKS_OUT_PATH" ]; then
+    # shellcheck source="$HERMAN_CODE_DIR/limericks_out.bash"
+    source "$LIMERICKS_OUT_PATH"
 fi
