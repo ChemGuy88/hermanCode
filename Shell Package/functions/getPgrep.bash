9ff606867c180199a409e5e48779bec5b0d1fb28
Herman <herman@herman-imac.attlocal.net>
2024-06-03 00:16:13 -0400

    Add feedback

--- Shell Package/functions/getPgrep.bash
+++ Shell Package/functions/getPgrep.bash
@@ -1,9 +1,19 @@
 
 getPgrep_Linux() {
+    if [ -z "$1" ] && [ -z "$2" ];
+    then
+        echo "You must provide both the user and keyword arguments for getPgrep"
+        return 1
+    fi
     pgrep -U "$1" -f "$2" | xargs --no-run-if-empty ps f -o lstart -o "%p %r %y %x %a" --sort +pid p
 }
 
 getPgrep_macOS() {
+    if [ -z "$1" ] && [ -z "$2" ];
+    then
+        echo "You must provide both the user and keyword arguments for getPgrep"
+        return 1
+    fi
     pgrep -U "$1" -f "$2" | xargs --no-run-if-empty ps -o lstart -o pid -o pgid -o tty -o time -o command
 }
 
