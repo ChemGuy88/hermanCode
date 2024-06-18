8f03fdc44891b1a3a5068713189e59de5eca6bf5
Herman Autore <herman@ufl.edu>
2024-06-18 17:05:06 -0400

    Call logout for Midas only on relevant operating systems.

--- Shell Package/logout.bash
+++ Shell Package/logout.bash
@@ -5,9 +5,29 @@ if [ -f "$LIMERICKS_OUT_PATH" ]; then
     source "$LIMERICKS_OUT_PATH"
 fi
 
-# For Midas
-LIMERICKS_OUT_PATH="$HERMANS_CODE_INSTALL_PATH/Shell Package/limericks_out_midas.bash"
-if [ -f "$LIMERICKS_OUT_PATH" ]; then
-    # shellcheck source="Shell Package/limericks_out_midas.bash"
-    source "$LIMERICKS_OUT_PATH"
+if [[ $OSTYPE == "darwin"* ]]; then
+    # :: macOS ::
+    :
+    if [[ "herman-imac.attlocal.net" == "$(hostname)" ]]; then
+        # :: macOS at home ::
+        # :: >>> macOS at home - Midas project >>> ::
+        LIMERICKS_OUT_PATH="$HERMANS_CODE_INSTALL_PATH/Shell Package/limericks_out_midas.bash"
+        if [ -f "$LIMERICKS_OUT_PATH" ]; then
+            # shellcheck source="Shell Package/limericks_out_midas.bash"
+            source "$LIMERICKS_OUT_PATH"
+        fi
+        # :: <<< macOS at home - Midas project <<< ::
+        :
+    elif [[ "AHC-Mac-Admins-MacBook-Pro.local" == "$(hostname)" ]]; then
+        # :: macOS at work ::
+        :
+    else
+        echo "Unsupported machine."
+    fi
+elif [[ $OSTYPE == "linux-gnu"* ]]; then
+    # :: Linux ::
+    :
+else
+    echo "Unsupported machine."
+    return
 fi
